f4bbf83a51f6defec4d1160de304dc7e6e548402
Waleed <virtualwaleed@gmail.com>
2021-11-12 14:49:12 +0500

    Fix Color

--- components/Thinkific.js
+++ components/Thinkific.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function Thinkific() {
     return (
-        <div>Thinkific</div>
+        <section className="bg-white dark:bg-gray-800">
+            <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+                <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+                    Thinkific
+                </h1>
+            </div>
+        </section>
     )
 }
