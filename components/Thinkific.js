83ce08b1288aeef7ec69b40ae14f3fd7c4daa0db
Waleed <virtualwaleed@gmail.com>
2021-11-15 10:48:21 +0500

    Add Thinkific

--- components/Thinkific.js
+++ components/Thinkific.js
@@ -3,10 +3,21 @@ import React from "react";
 export default function Thinkific() {
     return (
         <section className="bg-white dark:bg-gray-800">
-            <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-                <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
-                    Thinkific
-                </h1>
+            <header className="max-w-6xl mx-auto my-10 text-center bg-white dark:bg-gray-800">
+                <h3 className="text-3xl md:text-7xl font-black mx-20 my-10 md:leading-relaxed">
+                    Need custom features for your Thinkific school?
+                </h3>
+                <img
+                    className="block mx-auto w-24 h-22 md:w-48 md:h-46"
+                    src="/Thinkific-Experts-Badge.webp"
+                />
+            </header>
+            <div className="bg-[#F1F1F1] dark:bg-gray-900">
+                <div className="max-w-6xl mx-auto p-20 text-center dark:bg-gray-900">
+                    <p className="text-lg">
+                        As a certified Thinkific Development Expert, I can develop any custom feature for your Thinkific site. With excellent Frontend Development Skills I can create a signature experience for your students/customers which will help convert more and increase engagement with your content.
+                    </p>
+                </div>
             </div>
         </section>
     )
