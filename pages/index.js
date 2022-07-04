88b5662cf07c0078c3894cfe55cce0da9bd4d3cf
Waleed Shahid <iamwaleedshahid@gmail.com>
2022-07-05 00:49:35 +0500

    Remove Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
-import FeaturedProjects from "../components/FeaturedProjects";
-import Hero from "../components/Hero";
-import userData from "@constants/data";
-import AboutMe from "@components/AboutMe";
-import Contact from "@components/Contact";
+import Thinkific from "@components/Thinkific";
 
 export default function Home() {
   return (
@@ -13,15 +10,7 @@ export default function Home() {
       title="Waleed Shahid - Full-stack Developer"
       description="Full-stack Developer with 3+ Years Experience. Worked on 50+ projects, Top Rated Freelancer on Upwork, Thinkific Certified Expert."
     >
-      <Hero />
-      <div className="block py-6 md:-my-4 md:py-0">
-        <svg className="animate-bounce mx-auto w-6 h-6 text-amber-900" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-          <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-        </svg>
-      </div>
-      <AboutMe />
-      <FeaturedProjects />
-      <Contact bg="bg-white" />
+      <Thinkific />
     </ContainerBlock>
   );
 }
